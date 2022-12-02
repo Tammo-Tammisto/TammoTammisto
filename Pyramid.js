f4bc9f4dc94206ef44c92c30b5f7e690075ad59a
Tammo-Tammisto <113598068+Tammo-Tammisto@users.noreply.github.com>
2022-12-02 16:50:11 +0200

    Add files via upload

---
+++ Pyramid.js
@@ -0,0 +1,10 @@
+let rida=1
+function pyra(klotsArv) {
+    for (let i = 1; klotsArv >= i; i = i + rida){
+        rida++
+        n=i}
+    answerRida= rida-1
+    answerJaak = klotsArv-n
+    console.log("Saab teha " + answerRida + " realise torni")
+    console.log("Üle jääb " + answerJaak + " klotsi")}
+pyra(1372)
---
+++ pyramid ül2.js
@@ -0,0 +1,6 @@
+let rida = 1
+const klotsArv = 55
+for (let i = 1; klotsArv >= i; i = i + rida) {
+   console.log("*".repeat(rida))
+   rida++
+}
